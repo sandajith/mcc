3d6f91b5e8947b454d8887705b9312ab6941d5b1
neenurobin <neenurobin28@gmail.com>
2015-09-08 17:41:20 +0530

    pick up success page

--- app/code/local/Mycloset/Membership/controllers/PickupController.php
+++ app/code/local/Mycloset/Membership/controllers/PickupController.php
@@ -1,25 +1,19 @@
 <?php
-
 class Mycloset_Membership_PickupController extends Mage_Core_Controller_Front_Action {
 
-
-    public function indexAction() {
+    public function IndexAction() {
+        $this->loadLayout();
         if (!empty(Mage::app()->getRequest()->getPost())) {
             $userid = Mage::getSingleton('customer/session')->getId();
             $values = Mage::app()->getRequest()->getParams();
-
             $products = Mage::getModel('catalog/product')->getCollection()
                     ->addAttributeToFilter('customer_name', $userid);
-                    exit();
-            if (Mage::getSingleton('customer/session')->isLoggedIn() && count($products) > 0) {
-
+            if (Mage::getSingleton('customer/session')->isLoggedIn()) {
                 $final = array();
                 $i = 0;
                 foreach ($values['categoryname'] as $cat_id) {
-
                     $final[$i]['cat_id'] = $cat_id;
                     $final[$i]['quantity'] = $values['quantity'][$cat_id];
-
                     $i++;
                 }
                 $info = serialize($final);
@@ -30,15 +24,11 @@ class Mycloset_Membership_PickupController extends Mage_Core_Controller_Front_Ac
                 );
                 $model = Mage::getModel('membership/pickupdetails')->setData($data);
                 $model->save()->getId();
-               
-//               unset(Mage::app()->getRequest()->getPost());
-//                unset($_POST);
-                 $this->_redirect('mycloset/pickup/success');
+                $this->_redirect('mycloset/pickup/success');
             } else {
                 $this->_redirect('/membership/account/login/');
             }
         }
-        $this->loadLayout();
         $this->renderLayout();
     }
 
