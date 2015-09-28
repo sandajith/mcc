38fbcc922dee55e63201757a35739f99002a9ccf
neenurobin <neenurobin28@gmail.com>
2015-09-28 17:18:39 +0530

    mycloset_payment history insertion add primary key

--- app/code/local/Mycloset/Membership/Model/Mysql4/Paymenthistory.php
+++ app/code/local/Mycloset/Membership/Model/Mysql4/Paymenthistory.php
@@ -3,7 +3,7 @@
 class Mycloset_Membership_Model_Mysql4_Paymenthistory extends Mage_Core_Model_Mysql4_Abstract {
 
     public function _construct() {
-        $this->_init('membership/paymenthistory', 'payment_id');
+        $this->_init('membership/paymenthistory', 'payment_history_id');
     }
 
 }
