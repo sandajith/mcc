24ef36c26d23612194c226a0edbe5be545e792ac
neenurobin <neenurobin28@gmail.com>
2015-10-01 16:19:49 +0530

    uploading reworks

--- app/code/local/Mycloset/Membership/Block/Membership/List.php
+++ app/code/local/Mycloset/Membership/Block/Membership/List.php
@@ -1,14 +1,12 @@
 <?php
 
 class Mycloset_Membership_Block_Membership_List extends Mage_Core_Block_Template {
-    
+
     const PATH_GATE_URL = 'membership/general/gatewayurl';
     const PATH_API_LOGIN = 'membership/general/apilogin';
     const PATH_MER_EMAIL = 'membership/general/merchantmail';
     const PATH_TRANS_KEY = 'membership/general/transkey';
-   
-    
-   
+
     public function getMemberships() {
 
         $model = Mage::getModel('membership/types');
@@ -21,25 +19,41 @@ class Mycloset_Membership_Block_Membership_List extends Mage_Core_Block_Template
         return $mem;
     }
 
-    public function getChosenplan() {
-        $model = Mage::getModel('customer/customer')->load(Mage::getSingleton('customer/session')->getMemId());
-        $memtypeid = $model->getMemType();
-        $one = Mage::getModel('membership/types')->load($memtypeid);
-        $id = $one->getMembershipId();
-        $price = $one->getMembershipPrice();
-        $plan = $one->getMembershipType();
-        return $price.'-'.$plan.'-'.$id;
-    }
-    
-    public function getSettings() {
-     $setting['a'] = Mage::getStoreConfig(self::PATH_GATE_URL);
-     $setting['b'] = Mage::getStoreConfig(self::PATH_API_LOGIN);
-     $setting['c'] = Mage::getStoreConfig(self::PATH_MER_EMAIL);
-     $setting['d'] = Mage::getStoreConfig(self::PATH_TRANS_KEY);
-     //return Mage::getStoreConfig(self::PATH_TRANS_KEY);
-     return $setting;
+    public function getMembershipdetails() {
+
+        $customer_id = Mage::getSingleton('customer/session')->getMemId();
+        if(!$customer_id)
+        {
+           $customer_id = Mage::getSingleton('customer/session')->getCustomer()->getId();
+        }
      
+     
+            $customer = Mage::getModel('customer/customer')->load($customer_id);
+            $data = array();
+            $data['customer_group'] = $customer->getGroupId();
+            $data['customer_id'] = $customer->getId();
+            $customer_membership = Mage::getModel('membership/customermembership')->load($customer_id, 'customer_id');
+            $membership_id = $customer_membership->getMembershipId();
+            $one = Mage::getModel('membership/types')->load($membership_id);
+           $data['membership_id'] = $membership_id;
+          $data['membership_type'] = $one->getMembershipType();
+           $data['membership_price'] = $one->getMembershipPrice();
+            
+
+
+            return $data;
+       
+    }
+
+    public function getSettings() {
+        $setting['a'] = Mage::getStoreConfig(self::PATH_GATE_URL);
+        $setting['b'] = Mage::getStoreConfig(self::PATH_API_LOGIN);
+        $setting['c'] = Mage::getStoreConfig(self::PATH_MER_EMAIL);
+        $setting['d'] = Mage::getStoreConfig(self::PATH_TRANS_KEY);
+        //return Mage::getStoreConfig(self::PATH_TRANS_KEY);
+        return $setting;
     }
 
 }
+
 //Mage::getSingleton('customer/session')->getMemId();
