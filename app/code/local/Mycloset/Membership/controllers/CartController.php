5c8d5bddf1ff847975d16dadc7fdd3c3e4ca8e9c
neenurobin <neenurobin28@gmail.com>
2015-10-06 15:38:16 +0530

    mybag in progress

---
+++ app/code/local/Mycloset/Membership/controllers/CartController.php
@@ -0,0 +1,15 @@
+<?php
+
+class Mycloset_Membership_CartController extends Mage_Core_Controller_Front_Action {
+
+    //const PATH_GATE_THRESHOLD = 'membership/general/threshold';
+
+    public function indexAction() {
+        
+        $this->loadLayout();
+        $this->renderLayout();
+        
+      
+    }
+}
+    
